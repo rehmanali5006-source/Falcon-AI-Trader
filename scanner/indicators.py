e6b7f909e350eca7d6b4903a4ad88113cd53d664
rehmanali5006-source <rehmanali5006@gmail.com>
2026-07-09 19:45:47 +0500

    Update indicators.py

--- scanner/indicators.py
+++ scanner/indicators.py
@@ -3,27 +3,28 @@ import pandas as pd
 
 def add_indicators(df):
 
-    # EMA 20
-    df["EMA20"] = df["close"].ewm(span=20).mean()
-
-    # EMA 50
-    df["EMA50"] = df["close"].ewm(span=50).mean()
+    # EMA
+    df["EMA20"] = df["close"].ewm(span=20, adjust=False).mean()
+    df["EMA50"] = df["close"].ewm(span=50, adjust=False).mean()
 
     # RSI
     delta = df["close"].diff()
 
-    gain = delta.where(delta > 0, 0).rolling(14).mean()
-    loss = (-delta.where(delta < 0, 0)).rolling(14).mean()
+    gain = delta.clip(lower=0)
+    loss = -delta.clip(upper=0)
 
-    rs = gain / loss
+    avg_gain = gain.rolling(14).mean()
+    avg_loss = loss.rolling(14).mean()
+
+    rs = avg_gain / avg_loss
 
     df["RSI"] = 100 - (100 / (1 + rs))
 
     # MACD
-    ema12 = df["close"].ewm(span=12).mean()
-    ema26 = df["close"].ewm(span=26).mean()
+    ema12 = df["close"].ewm(span=12, adjust=False).mean()
+    ema26 = df["close"].ewm(span=26, adjust=False).mean()
 
     df["MACD"] = ema12 - ema26
-    df["MACD_SIGNAL"] = df["MACD"].ewm(span=9).mean()
+    df["MACD_SIGNAL"] = df["MACD"].ewm(span=9, adjust=False).mean()
 
     return df
